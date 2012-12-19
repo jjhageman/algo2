09876ec72c767212c7dc8f194f2592da565b2b84
Jeremy Hageman <jjhageman@gmail.com>
2012-12-19 00:38:15 -0800

    wip

--- ex2/clustering1.rb
+++ ex2/clustering1.rb
@@ -13,12 +13,13 @@ class Edge
 end
 
 class Graph
-  attr :edges
+  attr :edges, :points
 
   def initialize(file)
     @file = file
     @size = 0
     @edges = []
+    @points = Set.new
     populate
   end
   
@@ -26,6 +27,8 @@ class Graph
     csv = CSV.open(@file, 'r', col_sep: ' ', converters: :integer)
     @size = csv.shift
     csv.each do |row|
+      @points.add(row[0])
+      @points.add(row[1])
       @edges << Edge.new(*row)
     end
   end
@@ -34,21 +37,48 @@ end
 class Cluster
   attr :points
   
-  def initialize(point)
-    @points = Set.new(point)
+  def initialize(points)
+    @points = Set.new points
   end
 end
 
 class MaxSpacing
-  attr :graph
+  attr :graph, :clusters, :sorted_edges
 
   def initialize(file)
     @file = file
     @clusters = []
     @graph = Graph.new(file)
     @sorted_edges = @graph.edges.sort_by{|e| e.distance}
+    init_clusters
+  end
+
+  def init_clusters
+    @graph.points.each { |p| @clusters << Cluster.new([p]) }
+  end
+
+  def cluster(k)
+    while @clusters.size > k do
+      edge = @sorted_edges.shift
+      merge_clusters edge.p1, edge.p2
+    end
+    debugger
+    puts @sorted_edges.first.inspect
+  end
+
+  def merge_clusters(p1,p2)
+    c1 = @clusters.detect{|c| c.points.member?(p1)}
+    c2 = @clusters.detect{|c| c.points.member?(p2)}
+    unless c1 == c2
+      points = c1.points.merge(c2.points)
+      @clusters.delete(c1)
+      @clusters.delete(c2)
+      c = Cluster.new(points)
+      @clusters<<c
+    end
   end
 end
 
 ms=MaxSpacing.new('clustering1.txt')
+ms.cluster(4)
 puts 'done'
