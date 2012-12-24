1d24e4d02f36587dc9006592ab651158dfe5754c
Jeremy Hageman <jjhageman@gmail.com>
2012-12-23 23:14:10 -0800

    ex2q2

--- ex2/clustering2.rb
+++ ex2/clustering2.rb
@@ -44,26 +44,25 @@ class Edge
 end
 
 class Clustering
-  attr :edges, :node_index
+  attr :edges, :sorted_edges, :node_index, :clusters, :size
 
   def initialize(file)
     @file = file
-    #@nodes = []
     @size = 0
     @node_index = {}
-    #@values = Set.new
     @edges = []
+    @clusters = []
     init_nodes
     init_edges
+    @sorted_edges = @edges.sort_by{|e| e.distance}
   end
 
   def cluster
-    pbar = ProgressBar.new("clustering", @edges.size)
-    @edges.each do |e|
-      n1 = @node_index[e.p1].first
-      n2 = @node_index[e.p2].first
-      unless n1.find == n2.find
-        n1.union(n2)
+    pbar = ProgressBar.new("clustering", @sorted_edges.size)
+    @sorted_edges.each do |e|
+      unless e.p1.find == e.p2.find
+        e.p1.union(e.p2)
+        @size -= 1
       end
       pbar.inc
     end
@@ -72,23 +71,24 @@ class Clustering
 
   def init_edges
     pbar = ProgressBar.new("initializing edges", @node_index.size)
-    @node_index.keys.each do |n|
-      dev = []
+    @node_index.each do |k,v|
+      if v.size > 1
+        puts v.size
+        o = v.first
+        v[1..-1].each do |j|
+          @edges << Edge.new(o,j,0)
+        end
+      end
+      n=k
       n.size.times do |i|
         temp = String.new n
         temp[i] = (n[i] == '0') ? '1' : '0'
-        dev << temp
+        (@edges << Edge.new(v.first,@node_index[temp].first,1)) if @node_index.has_key?(temp)
         n.size.times do |j|
           next if j == i
           temp2 = String.new temp
           temp2[j] = (temp[j] == '0') ? '1' : '0'
-          dev << temp2
-        end
-      end
-
-      dev.each do |d|
-        if @node_index.has_key?(d)
-          @edges << Edge.new(n,d,1)
+          (@edges << Edge.new(v.first,@node_index[temp2].first,2)) if @node_index.has_key?(temp2)
         end
       end
       pbar.inc
@@ -96,39 +96,18 @@ class Clustering
     pbar.finish
   end
 
-  def dist_less_two?(a,b)
-    dist = 0
-    24.times do |i|
-      dist += 1 if a[i] != b[i]
-      if dist > 2
-        return false
-      end
-    end
-    true
-  end
-
-  def str_distance(a,b)
-   int_distance(a.to_i(2),b.to_i(2)) 
-  end
-
-  def int_distance(a,b)
-    (a^b).to_s(2).count("1")
-  end
-
   def init_nodes
     csv = CSV.open(@file, 'r', col_sep: ' ', converters: :integer)
-    @size = csv.shift
-    pbar = ProgressBar.new("reading file", @size[0])
+    @size = csv.shift[0]
+    pbar = ProgressBar.new("reading file", @size)
     csv.each do |row|
       str = row.compact.join
       node = Node.new(str)
       if @node_index[str].nil?
         @node_index[str] = [node]
       else
-        @node_index[str].first.union(node)
         @node_index[str] << node
       end
-      #@nodes << Node.new(str)
       pbar.inc
     end
     pbar.finish
@@ -137,8 +116,4 @@ end
 
 c = Clustering.new('clustering2.txt')
 c.cluster
-p=Set.new
-c.node_index.each {|k,v| v.each {|x| p.add(x.parent)}}
-puts "#{p.size} clusters"
-debugger
-puts 'Done'
+puts "#{c.size} clusters"
