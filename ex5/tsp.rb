815530849140dc273463d3a604ee0b0dc11e20c6
Jeremy Hageman <jhageman@affinitylabs.com>
2013-01-25 17:06:14 -0800

    test data

--- ex5/tsp.rb
+++ ex5/tsp.rb
@@ -4,28 +4,43 @@ require 'set'
 require 'progressbar'
 
 class City
-  attr :x, :y
+  attr :x, :y, :id
 
-  def initialize(x,y)
+  def initialize(id,x,y)
+    @id = id
     @x = x
     @y = y
   end
 end
 
 class Tsp
-  attr :cities, :A, :S
+  attr :cities, :A, :D
 
   def initialize(file)
     @file = file
     @cities = []
+    @A = {}
+    @D = {}
     read_data
+    compute_distances
   end
 
   def read_data
-    csv = CSV.open(@file, 'r', col_sep: ' ', converters: :integer)
-    @num_cities = csv.shift.first
-    csv.each do |row|
-      @cities << City.new(*row)
+    csv = CSV.open(@file, 'r', col_sep: ' ', converters: :float)
+    @num_cities = csv.shift.first.to_i
+    csv.each.with_index do |row,i|
+      @cities << City.new(i+1,*row)
+    end
+  end
+
+  def compute_distances
+    @cities.each do |c1|
+      @D[c1.id] = {c1.id => 0}
+      (@cities-[c1]).each do |c2|
+        d = (c1.x-c2.x)**2 + (c1.y-c2.y)**2
+        d = Math.sqrt(d)
+        @D[c1.id][c2.id] = d
+      end
     end
   end
 
@@ -43,27 +58,34 @@ class Tsp
   end
 
   def min_cost
-    @A = Array.new(@num_cities) {[0]}
     pbar = ProgressBar.new("min cost", @num_cities)
-    (2..@num_cities).each do |m|
-      first_binary = (('0'*(24-m))+('1'*m)).to_i(2)
-      last_binary = (('1'*m)+('0'*(24-m))).to_i(2)
+    i = @num_cities-1
+    (1..i).each do |m|
+      first_binary = (('0'*(i-m))+('1'*m)).to_i(2)
+      last_binary = (('1'*m)+('0'*(i-m))).to_i(2)
       while first_binary <= last_binary do
-        ('%023b'%first_binary).split("").reverse.each.with_index do |c,position|
+        ("%0#{i}b"%first_binary).split("").reverse.each.with_index do |c,position|
           if c == '1'
             j = position+1
             s_sub_j = first_binary & ~(1<<position)
-            min = Float::INFINITY
-            ('%023b'%s_sub_j).split("").reverse.each.with_index do |s,p|
-              if s == '1'
-                k = s+1
-                temp = (k == 1) ? Float::INFINITY : (@A[s_sub_j][k] )#+ dist(k,j))
-                debugger unless temp
-                min = temp if temp < min
+            if s_sub_j == 0
+              @A[first_binary] = {(j+1) => @D[1][j+1]}
+            else
+              min = Float::INFINITY
+              ("%0#{i}b"%s_sub_j).split("").reverse.each.with_index do |s,p|
+                if s == '1'
+                  k = p+1
+                  temp = (k == 1) ? Float::INFINITY : (@A[s_sub_j][k+1] + @D[k+1][j+1])
+                  debugger unless temp
+                  min = temp if temp < min
+                end
+              end
+              if @A[first_binary]
+                @A[first_binary][j+1] = min
+              else
+                @A[first_binary] = {(j+1) => min}
               end
             end
-            debugger unless  @A[first_binary]
-            @A[first_binary][j] = min
           end
         end
         first_binary = next_pattern first_binary
@@ -71,7 +93,16 @@ class Tsp
       pbar.inc
     end
     pbar.finish
+
+    min = Float::INFINITY
+    n_index = ('1'*i).to_i(2)
+    (2..@num_cities).each do |j|
+      temp = @A[n_index][j] + @D[1][j]
+      min = temp if temp < min
+    end
+    min
   end
 end
 mc = Tsp.new('tsp.txt').min_cost
+puts "Min travel cost: #{mc}"
 puts 'Done.'
